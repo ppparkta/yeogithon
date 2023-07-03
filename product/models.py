34c2df35bb9aa35c7c5e0d46dcd5d55d81ca8c63
yushinc <cys74351@gmail.com>
2023-07-03 16:12:55 +0900

    feat: 폼 로직 변경

--- product/models.py
+++ product/models.py
@@ -5,6 +5,3 @@ class Product(models.Model):
     productPrice = models.IntegerField() # 가격
     productImage = models.ImageField(upload_to='', blank=False) # 상품 사진
     productTemp = models.BooleanField(default=True) # True -> ice, False -> hot, default 값은 ice로
-
-    def __str__(self):
-        return f'{self.productName}'
--- product/views.py
+++ product/views.py
@@ -1,25 +1,90 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from .forms import ProductForm
 from .models import Product
+
+
 def product_list(request):
     products = Product.objects.all().order_by('-pk')
     return render(request, 'product/product_list.html', context={'products': products})
 
+
+# def create_product(request):
+#     if request.method == "POST":
+#         form = ProductForm(request.POST, request.FILES)
+#
+#         if form.is_valid():
+#             product = form.save(commit=False)
+#             product.save()
+#             return redirect(reverse('product:product_list'))
+#
+#     else:
+#         form = ProductForm()
+#
+#     return render(request, 'product/create_product.html', context={'form': form})
+#
+# # 상품 수정 (권한 필요)
+# def edit_product(request, pk):
+#     swuni = request.user  # 현재 로그인한 사용자 정보 가져오기
+#
+#     if not swuni.is_authenticated:  # 권한이 없다면
+#         return render(request, 'product/401.html', status=401)  # 401 이동
+#
+#     product = Product.objects.get(id=pk)
+#
+#     if request.method == "POST":
+#         form = ProductForm(request.POST, instance=product)
+#
+#         if form.is_valid():
+#             product = form.save(commit=False)
+#             product.save()
+#             return redirect(reverse('product:product_list'))
+#
+#     else:
+#         form = ProductForm()
+#
+#     return render(request, 'product/create_product.html', context={'form': form})
+#
+# # 상품 삭제 (권한 필요)
+# def delete_product(request, pk):
+#     swuni = request.user  # 현재 로그인한 사용자 정보 가져오기
+#
+#     if not swuni.is_authenticated:  # 권한이 없다면
+#         return render(request, 'product/401.html', status=401)  # 401 이동
+#
+#     if request.method == "POST":
+#         product = Product.objects.get(id=pk)
+#         product.delete()
+#         return redirect(reverse('product:product_list'))
+#
+#     return render(request, 'product/failDelete.html')
+
+# ---------------- form 미사용 ------------------- #
+# 상품 생성 (권한 필요)
 def create_product(request):
+    swuni = request.user  # 현재 로그인한 사용자 정보 가져오기
+
+    if not swuni.is_authenticated:  # 권한이 없다면
+        return render(request, 'product/401.html', status=401)  # 401 이동
+
     if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
+        productName = request.POST.get('productName')
+        productPrice = request.POST.get('productPrice')
+        productImage = request.FILES.get('productImage')
+        # productTemp = request.POST.get('productTemp')
 
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.save()
-            return redirect(reverse('product:product_list'))
+        Product.objects.create(
+            productName=productName,
+            productPrice=productPrice,
+            productImage=productImage,
+            # productTemp=productTemp
+        )
+        return redirect(reverse('product:product_list'))
 
-    else:
-        form = ProductForm()
+    else:  # GET 요청이면
+        return render(request, 'product/create_product.html')
 
-    return render(request, 'product/create_product.html', context={'form': form})
 
 # 상품 수정 (권한 필요)
 def edit_product(request, pk):
@@ -28,20 +93,26 @@ def edit_product(request, pk):
     if not swuni.is_authenticated:  # 권한이 없다면
         return render(request, 'product/401.html', status=401)  # 401 이동
 
-    product = Product.objects.get(id=pk)
+    product = get_object_or_404(Product, pk=pk)
 
     if request.method == "POST":
-        form = ProductForm(request.POST, instance=product)
+        productName = request.POST.get('productName')
+        productPrice = request.POST.get('productPrice')
+        productImage = request.FILES.get('productImage')
+        #productTemp = request.POST.get('productTemp')
 
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.save()
-            return redirect(reverse('product:product_list'))
+        # 데이터 변경
+        product.productName = productName
+        product.productPrice = productPrice
+        product.productImage = productImage
+        #product.productTemp = productTemp
 
-    else:
-        form = ProductForm()
+        product.save()
 
-    return render(request, 'product/create_product.html', context={'form': form})
+        return redirect(reverse('product:product_list'))
+
+    return render(request, 'product/create_product.html', context={'product': product})
+
 
 # 상품 삭제 (권한 필요)
 def delete_product(request, pk):
@@ -56,73 +127,3 @@ def delete_product(request, pk):
         return redirect(reverse('product:product_list'))
 
     return render(request, 'product/failDelete.html')
-
-# ---------------- form 미사용 ------------------- #
-# 상품 생성 (권한 필요)
-# def create_product(request):
-#     swuni = request.user # 현재 로그인한 사용자 정보 가져오기
-#
-#     if not swuni.is_authenticated: # 권한이 없다면
-#         return render(request, 'product/401.html', status=401) # 401 이동
-#
-#     if request.method == "POST":
-#         productName = request.POST.get('productName')
-#         productPrice = request.POST.get('productPrice')
-#         productImage = request.FILES.get('productImage')
-#         # productTemp = request.POST.get('productTemp')
-#
-#         Product.objects.create(
-#             productName=productName,
-#             productPrice=productPrice,
-#             productImage=productImage,
-#             # productTemp=productTemp
-#         )
-#         return redirect(reverse('product:product_list'))
-#
-#     else: # GET 요청이면
-#         return render(request, 'product/create_product.html')
-#
-#
-# # 상품 수정 (권한 필요)
-# def edit_product(request, pk):
-#     swuni = request.user  # 현재 로그인한 사용자 정보 가져오기
-#
-#     if not swuni.is_authenticated:  # 권한이 없다면
-#         return render(request, 'product/401.html', status=401)  # 401 이동
-#
-#     product = Product.get_object_or_404(id=pk)
-#
-#
-#     if request.method == "POST":
-#         productName = request.POST.get('productName')
-#         productPrice = request.POST.get('productPrice')
-#         productImage = request.FILES.get('productImage')
-#         # productTemp = request.POST.get('productTemp')
-#
-#         # 데이터 변경
-#         product.productName = productName,
-#         product.productPrice = productPrice,
-#         product.productImage = productImage
-#         # product.productTemp = productTemp
-#
-#         product.save()
-#
-#         return redirect(reverse('product:product_list'))
-#
-#     # return render(request, 'product/create_product.html', context={'form': form})
-#     return redirect('product/create_product.html', product.pk)
-#
-#
-# # 상품 삭제 (권한 필요)
-# def delete_product(request, pk):
-#     swuni = request.user  # 현재 로그인한 사용자 정보 가져오기
-#
-#     if not swuni.is_authenticated:  # 권한이 없다면
-#         return render(request, 'product/401.html', status=401)  # 401 이동
-#
-#     if request.method == "GET":
-#         product = Product.objects.get(id=pk)
-#         product.delete()
-#         return redirect(reverse('product:product_list'))
-#
-#     return render(request, 'product/failDelete.html')
